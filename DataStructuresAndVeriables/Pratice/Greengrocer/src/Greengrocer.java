89bee881bdde1b02252c32775fdbf42a28b556c0
Hüseyin idin <huseyinidinn@gmail.com>
2022-01-13 00:28:30 +0300

    Workshop

--- DataStructuresAndVeriables/Pratice/Greengrocer/src/Greengrocer.java
+++ DataStructuresAndVeriables/Pratice/Greengrocer/src/Greengrocer.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Greengrocer {
+import java.util.Scanner;
+
+public class Greengrocer {
+
+    public static void main(String[] args) {
+
+        double p,a,t,b,e, total;
+        double pear = 2.14, apple = 3.67, tomato = 1.11, banana = 0.95, eggplant = 5.00;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("How much pear? ");
+        p = input.nextDouble();
+
+        System.out.print("How much apple? ");
+        a = input.nextDouble();
+
+        System.out.print("How much tomato? ");
+        t = input.nextDouble();
+
+        System.out.print("How much banana? ");
+        b = input.nextDouble();
+
+        System.out.print("How much eggplant? ");
+        e = input.nextDouble();
+
+        total = (pear * p) + (apple * a) + (tomato * t) + (banana * b) + (eggplant * e);
+
+        System.out.println("Total Price: " + total);
+    }
 }
