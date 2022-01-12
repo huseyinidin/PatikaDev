bcdc7aeb283c51ca3a32035a73ddcd1903760c9b
Hüseyin idin <huseyinidinn@gmail.com>
2022-01-12 23:52:04 +0300

    Workshop

--- DataStructuresAndVeriables/Pratice/BodyMassIndex/src/Bmi.java
+++ DataStructuresAndVeriables/Pratice/BodyMassIndex/src/Bmi.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Bmi {
+import java.util.Scanner;
+
+public class Bmi {
+
+    public static void main(String[] args) {
+
+        double height, weight, bmi;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your height: ");
+        height = input.nextDouble();
+
+        System.out.print("please enter your weight: ");
+        weight = input.nextDouble();
+
+        bmi = weight / (height * height);
+
+        System.out.println("Your body mass index: " + bmi);
+
+    }
 }
