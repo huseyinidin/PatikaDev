87b6d7009abd04dc4ffd8c74443e7b204b9a0eea
Hüseyin idin <huseyinidinn@gmail.com>
2022-01-12 20:19:55 +0300

    Readme added

---
+++ DataStructuresAndVeriables/Pratice/Hypotenuse/README.md
@@ -0,0 +1,13 @@
+﻿#Dik Üçgende Hipotenüs Bulan Program
+
+Java ile kullanıcıdan dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yazın.
+
+##Ödev
+Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.
+
+**Formül**
+Üçgenin Çevresi = 2u
+
+u = (a+b+c) / 2
+
+Alan * Alan = u * (u - a) * (u - b) * (u - c)
---
+++ DataStructuresAndVeriables/Pratice/Hypotenuse/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Hypotenuse {
+    public static void main(String[] args) {
+
+        int a, b;
+        double c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first edge: ");
+        a = input.nextInt();
+
+        System.out.print("Enter the second edge: ");
+        b = input.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.println("Hypotenuse: " + c);
+
+        /*Benim yazdığım yol
+
+        c = (a*a) + (b*b);
+        result = c/5;
+        System.out.println(result);*/
+    }
+}
