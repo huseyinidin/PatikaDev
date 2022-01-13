adc13cddbf8926dd120d497692ec736674d6fcd2
Hüseyin idin <huseyinidinn@gmail.com>
2022-01-13 18:10:30 +0300

    Calculator

--- ConditionalStatementsAndScope/Calculator/src/Calculator.java
+++ ConditionalStatementsAndScope/Calculator/src/Calculator.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Calculator {
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int num1, num2, select;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        num1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        num2 = input.nextInt();
+
+        System.out.println("1- Plus\n2- Minus\n3- Multiply\n4- Divided");
+        System.out.print("Your coise: ");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println("Plus: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Minus: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Multiply: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Divided: " + (num1 / num2));
+                } else {
+                    System.out.println("a number can't be divided by zero");
+                }
+                break;
+            default:
+                System.out.println("You entered incorrectly");
+        }
+    }
 }
--- ConditionalStatementsAndScope/Calculator/src/Calculatorr.java
+++ ConditionalStatementsAndScope/Calculator/src/Calculatorr.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Calculatorr {
+import java.util.Scanner;
+
+public class Calculatorr {
+
+    public static void main(String[] args) {
+
+        int num1, num2, select;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        num1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        num2 = input.nextInt();
+
+        System.out.println("1- Plus\n2- Minus\n3- Multiply\n4- Divided");
+        System.out.print("Your coise: ");
+        select = input.nextInt();
+
+        if (select == 1) {
+            System.out.println("Plus: " + (num1 + num2));
+        } else if (select == 2) {
+            System.out.println("Minus: " + (num1 - num2));
+        } else if (select == 3) {
+            System.out.println("Minus: " + (num1 - num2));
+        } else if (select == 4) {
+            if (num2 != 0) {
+                System.out.println("Divided: " + (num1 / num2));
+            } else {
+                System.out.println("a number can't be divided by zero");
+            }
+        } else {
+            System.out.println("You entered incorrectly");
+        }
+    }
 }
