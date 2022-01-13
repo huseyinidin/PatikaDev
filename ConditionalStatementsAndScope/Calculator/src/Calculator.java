bb2eb6f82b9ebcef9950100ca7df8be6ba4a3d44
Hüseyin idin <huseyinidinn@gmail.com>
2022-01-13 18:12:25 +0300

    Workshop

--- ConditionalStatementsAndScope/Calculator/src/Calculator.java
+++ ConditionalStatementsAndScope/Calculator/src/Calculator.java
@@ -31,7 +31,7 @@ public class Calculator {
                 if (num2 != 0) {
                     System.out.println("Divided: " + (num1 / num2));
                 } else {
-                    System.out.println("a number can't be divided by zero");
+                    System.out.println("a number can't be divided by ");
                 }
                 break;
             default:
