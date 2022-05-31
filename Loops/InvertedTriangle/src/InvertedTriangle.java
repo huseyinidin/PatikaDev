eacf2bb4c370ec65ca6bc8aa943fb339efad83c8
Hüseyin idin <huseyinidinn@gmail.com>
2022-05-31 22:02:21 +0300

    HomeWork

--- Loops/InvertedTriangle/src/InvertedTriangle.java
+++ Loops/InvertedTriangle/src/InvertedTriangle.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class InvertedTriangle {
     public static void main(String[] args) {
 
-        int space, star, line;
+        int space = 0, star = 2, line;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
         line = input.nextInt();
-        star = 2;
-        space = 0;
 
         for (int i = line; i > 0; i--) {
             for (int j = space; j > 0; j--) {
