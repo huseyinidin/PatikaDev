09c2396d8ebb1e93bcab6643cc208bc5018b6cc9
Hüseyin idin <huseyinidinn@gmail.com>
2022-06-24 00:25:23 +0300

    Practice

---
+++ Arrays/HarmonicNumberArray/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] list = {1, 2, 3, 4, 5};
+        double sum = 0.0;
+        for (int i : list) {
+            sum += i;
+        }
+
+        double average = sum / list.length;
+        System.out.println(average);
+    }
+}
