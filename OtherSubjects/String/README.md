b64ea12db53ac27c53ba220c492660cf145e37ab
Hüseyin idin <huseyinidinn@gmail.com>
2022-06-28 23:52:50 +0300

    Readme Added

---
+++ OtherSubjects/String/README.md
@@ -0,0 +1,12 @@
+# Math
+
+## String Sýnýfý ve Metotlarý
+
+Java'da String sýnýflarý char türünden verilerden oluþmuþ bir kümedir yani karakterlerden oluþur. Java iþlerimizi kolaylaþtýrmak ve String ifadelerle iþlem yapmak için String sýnýfýný sunar.
+
+### String Sýnýfý Metotlarý
+
+
+![String_Cheat_Sheet](https://raw.githubusercontent.com/Kodluyoruz/taskforce/main/java101/string-sinifi/figures/1-1.jpg)
+![String_Cheat_Sheet](https://raw.githubusercontent.com/Kodluyoruz/taskforce/main/java101/string-sinifi/figures/2-2.jpg)
+
---
+++ OtherSubjects/String/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "pathway";
+        String string = "patHWay";
+        String s1 = "";
+        String s2 = "Pathway";
+        String s3 = "    Pathway    ";
+
+        System.out.println("index number: " + str.charAt(0));
+        System.out.println("index number: " + str.charAt(4));
+        System.out.println("find ASCII code: " + str.codePointAt(2) + " index number");
+        System.out.println("find ASCII code: " + str.codePointBefore(2) + " before index number");
+        System.out.println("compare to: " + str.compareTo(string) + " the letters are different");
+        System.out.println("compare To Ignore Case: " + str.compareToIgnoreCase(string) + " capital letters doesn't matter");
+        System.out.println("concat: " + str.concat(".dev"));
+        System.out.println("contains: " + str.contains("hw") + " return true or false");
+        System.out.println("containsEquals: " + str.contentEquals("pathway") + " return true or false");
+        System.out.println("containsEquals: " + str.contentEquals("way") + " return true or false");
+        System.out.println("endWith: " + str.endsWith("ay") + " return true or false");
+        System.out.println("endWith: " + str.endsWith("ya") + " return true or false");
+        System.out.println("startsWith: " + str.startsWith("p") + " return true or false");
+        System.out.println("startsWith: " + str.startsWith("a") + " return true or false");
+        System.out.println("equals: " + str.equals("pathway") + " the letters are same");
+        System.out.println("equals Ignore Case: " + str.equalsIgnoreCase("PathWay") + " capital letters doesn't matter");
+        System.out.println("index number: " + str.indexOf('p'));
+        System.out.println("is Empty: " + s1.isEmpty() + " return true or false");
+        System.out.println("is Empty: " + s2.isEmpty() + " return true or false");
+        System.out.println("subString: " + str.substring(4));
+        System.out.println("subString: " + str.substring(2,4));
+        System.out.println("to Lower Case: " + string.toLowerCase());
+        System.out.println("to Upper Case: " + str.toUpperCase());
+        System.out.println("trim: " + s3.trim());
+
+
+    }
+}
