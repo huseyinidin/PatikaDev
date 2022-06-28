04862dd96cecfec1530308f9d5aaa912c1d965e7
Hüseyin idin <huseyinidinn@gmail.com>
2022-06-28 22:17:20 +0300

    Math

---
+++ OtherSubjects/Math/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.max(100,200));
+        System.out.println(Math.min(100,200));
+        System.out.println(Math.sqrt(5));
+        System.out.println(Math.pow(5,2));
+    }
+}
